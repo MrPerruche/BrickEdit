02665ec6bc76ebbff450ec992e3738ac567c152c
FateUnix29 <fateofcomputer@proton.me>
2025-10-22 14:09:32 -0700

    really really broken messy wip deserialization dont look

--- src/brickedit/brv.py
+++ src/brickedit/brv.py
@@ -131,7 +131,7 @@ class BRVFile:
                 prop_serialization_class = _p.pmeta_registry.get(prop)
                 if prop_serialization_class is None:
                     raise _e.BrickError(f"Property {prop!r} from brick {brick!r}"
-                                        "does not have any serialization class registed.")
+                                        "does not have any serialization class registered.")
 
                 # Put in the lists if it's not already
                 if prop not in prop_to_index:
@@ -252,3 +252,127 @@ class BRVFile:
             buffer.extend(subbuffer)
 
         return buffer
+
+
+
+    def deserialize(self, buffer: bytes) -> None:
+        """Deserialize a bytearray into this vehicle.
+
+        Args:
+            buffer (bytes): Bytearray to deserialize
+        """
+
+        self.bricks.clear()
+
+        self.version = struct.unpack('B', buffer[0:1])[0]
+
+        num_bricks = struct.unpack('<H', buffer[1:3])[0]
+
+        print(f"{self.version=}")
+        print(f"{num_bricks=}")
+
+        types = set()
+        index_to_type = {} # proceeds to not even use this
+        index_to_prop = {}
+        index_to_value = [{}]
+        serialized_to_indexes = []
+
+        num_types = struct.unpack('<H', buffer[3:5])[0]
+        print(f"{num_types=}")
+        num_properties = struct.unpack('<H', buffer[5:7])[0]
+        print(f"{num_properties=}")
+
+        pointer = 7
+        tmp_pointer = pointer
+
+        for _ in range(num_types):
+            type_name_len = struct.unpack('B', buffer[pointer: pointer + 1])[0]
+            pointer += 1
+            type_name = buffer[pointer : pointer + type_name_len].decode('ascii')
+            types.add(type_name)
+            pointer += type_name_len
+
+        print(f"{types=}")
+
+        #property_index = 0
+        for _ in range(num_properties):
+            print(f"\nproperty... {pointer=}, {tmp_pointer=}, {pointer-tmp_pointer=}\n")
+            # one byte for how long the property name is
+            prop_name_len = struct.unpack('B', buffer[pointer: pointer + 1])[0]
+            pointer += 1
+            # the actual name of this property
+            prop_name = buffer[pointer : pointer + prop_name_len].decode('ascii')
+            # i have no idea what im doing with index_to_xyz anymore...! just trying to reverse serialize
+            index_to_prop[len(index_to_prop)] = prop_name
+            index_to_value.append({})
+            pointer += prop_name_len
+
+            # property class that we need to use
+            prop_serialization_class = _p.pmeta_registry.get(prop_name)
+            if prop_serialization_class is None:
+                raise _e.BrickError(f"Property '{prop_name}' does not exist in the registry.")
+
+            print(f"{prop_serialization_class=}")
+
+            # the number of values the header tells us to expect.
+            num_values_expected = struct.unpack('<H', buffer[pointer: pointer + 2])[0]
+            pointer += 2
+
+            print(f"{num_values_expected=}")
+
+            # total length of all of the values.
+            total_len_values = struct.unpack('<I', buffer[pointer: pointer + 4])[0]
+            pointer += 4
+
+            print(f"{total_len_values=}")
+
+            # read way past that to get to the actual uint16[] of their lengths
+
+            value_lens = []
+
+            if num_values_expected > 1:
+                tmp_pointer = pointer + total_len_values
+                # we currently have no idea how long the values are
+                # we can ... probably ... assume that no value is 0 bytes long, therefore,
+                # we can make an easy check to see if the next 2 bytes == \x00\x00
+                print(f"{buffer[tmp_pointer:tmp_pointer+2]=}")
+                if buffer[tmp_pointer:tmp_pointer+2] == b'\x00\x00':
+                    # if so, we can just start at the next 2 bytes and read num_values_expected uint16s...
+                    tmp_pointer += 2
+                    for _ in range(num_values_expected):
+                        value_lens.append(struct.unpack('<H', buffer[tmp_pointer:tmp_pointer + 2])[0])
+                        tmp_pointer += 2
+                else:
+                    # if not, then everyone is the same length. become an int.
+                    value_lens = struct.unpack('<H', buffer[tmp_pointer:tmp_pointer + 2])[0]
+                    tmp_pointer += 2
+
+            else:
+                value_lens = total_len_values # The optimized nature of a BRV file
+                # means the uint16 array simply DOESN'T EXIST if there is only one value.
+
+            print(f"{value_lens=}")
+
+
+            # i took it that im not supposed to iterate over values here, so i commented it. this is what i *was* doing though.
+            #for i in range(num_values_expected):
+            #    if isinstance(value_lens, int):
+            #        buffer_slice = buffer[pointer: pointer + value_lens]
+            #        pointer += value_lens
+            #    else:
+            #        buffer_slice = buffer[pointer: pointer + value_lens[i]]
+            #        pointer += value_lens[i]
+            #    deserialized_value = prop_serialization_class.deserialize(buffer_slice, self.version)
+            #    print(f"{deserialized_value=}")
+            #    if prop_name not in index_to_value[-1]:
+            #        index_to_value[-1][prop_name] = deserialized_value
+            #    else:
+            #        # ... assume we're adding to a list
+            #        if not isinstance(index_to_value[-1][prop_name], list):
+            #            index_to_value[-1][prop_name] = [index_to_value[-1][prop_name]]
+            #        index_to_value[-1][prop_name].append(deserialized_value)
+            print(f"moving tm {pointer=} {tmp_pointer=}")
+            pointer = tmp_pointer if num_values_expected > 1 else pointer
+
+            print(f"{index_to_value=}")
+
